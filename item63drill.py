009daea99df231f41e0907029d46c7009b6af5f4
Daniel Beyer <dan.beyer1337@gmail.com>
2018-05-25 15:52:11 -0700

    Add files via upload

---
+++ item63drill.py
@@ -0,0 +1,18 @@
+import shutil
+
+
+shutil.move(r'C:\Users\Student\Desktop\Folder A\Random1.txt', \
+            r'C:\Users\Student\Desktop\Folder B')
+print(r'C:\Users\Student\Desktop\Folder A\Random1.txt')
+
+shutil.move(r'C:\Users\Student\Desktop\Folder A\Random2.txt', \
+            r'C:\Users\Student\Desktop\Folder B')
+print(r'C:\Users\Student\Desktop\Folder A\Random2.txt')
+
+shutil.move(r'C:\Users\Student\Desktop\Folder A\Random3.txt', \
+            r'C:\Users\Student\Desktop\Folder B')
+print(r'C:\Users\Student\Desktop\Folder A\Random3.txt')
+
+shutil.move(r'C:\Users\Student\Desktop\Folder A\Random4.txt', \
+            r'C:\Users\Student\Desktop\Folder B')
+print(r'C:\Users\Student\Desktop\Folder A\Random4.txt')
